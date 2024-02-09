645b2c94159f47bd9082e12c88a73c7f16f071bf
Azra <yukselazrael@gmail.com>
2024-02-09 11:53:44 +0300

    Added Project

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FaktoriyelHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+
+            Console.WriteLine("Sonuc1: " +faktoriyeFunction(n));
+            Console.WriteLine("Sonuc2: " +faktoriyeFunction2(n));
+        }
+
+        static long faktoriyeFunction(int number)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        static long faktoriyeFunction2(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * faktoriyeFunction2(number - 1);
+            }
+        }
+    }
+}
